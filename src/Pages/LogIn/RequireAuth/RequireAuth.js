3ebae34b3e354e635a9d521dee435a55dd937853
coderowson <runurowson@gmail.com>
2022-04-17 23:22:12 +0600

    requireAuth

--- src/Pages/LogIn/RequireAuth/RequireAuth.js
+++ src/Pages/LogIn/RequireAuth/RequireAuth.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Children } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../../../firbaseInit';
+import Spinnerr from '../Spinner/Spinnerr';
 
-const RequireAuth = () => {
-    return (
-        <div>
-
-        </div>
-    );
+const RequireAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth)
+    console.log('inside require auth', user);
+    const location = useLocation()
+    if (loading)
+        return <Spinnerr></Spinnerr>
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace />
+    }
+    return children
 };
 
 export default RequireAuth;
