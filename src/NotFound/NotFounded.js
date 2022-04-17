4f5c44b1596ec2835aadbd365be5d5b678ea786c
coderowson <runurowson@gmail.com>
2022-04-17 14:42:46 +0600

    not founded route

--- src/NotFound/NotFounded.js
+++ src/NotFound/NotFounded.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import sleeping from '../images/404.jpg'
 const NotFounded = () => {
     return (
         <div>
-
+            <h2 className="text-primary text-center">Pages are not founded</h2>
+            <img className='w-100' src={sleeping} alt="" />
         </div>
     );
 };
